6c59dd9fde11cbce896f6ccdc6f94a89a2c78e13
Eli Verbrugge <eli.verbrugge@gmail.com>
2021-07-21 16:28:47 -0500

    Updating main page to be responsive for mobile. Fixing some wording in projects

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,22 +1,44 @@
 import React from "react"
+import MediaQuery from 'react-responsive';
 import Typist from 'react-typist';
 import Links from "../components/Links"
 import Me from "../images/me.png"
 
 const Intro = () => (
-  <div style={{textAlign: "center", marginTop: "10%", marginLeft: "10%", marginRight: "10%"}}>
+  <div>
+      <MediaQuery maxDeviceWidth={1224}>
+      <div style={{backgroundColor: "rgba(128, 128, 128, .6)", textAlign: "center", marginTop: "10%", marginLeft: "10%", marginRight: "10%"}}>
       <Typist cursor={{show: false}}>
-      <h1>Hello there!</h1>
-      </Typist>
-      <br/>
-      <br/>
-      <h3 style={{}}>
-        I'm Eli Verbrugge <img src={Me} alt="Avatar" class="avatar"/>, a Computer Science and Computer Engineering graduate interested in building cool tech whether it be
-        robots, awesome applications to control them or providing cool data insights into tech.
-      </h3>
-      <br />
-      <br />
-      <Links />
+        <h1>Hello there!</h1>
+        </Typist>
+        <br/>
+        <br/>
+        <h3 style={{}}>
+          I'm Eli Verbrugge <img src={Me} alt="Avatar" class="avatar"/>, a Computer Science and Computer Engineering graduate interested in building cool tech whether it be
+          robots, awesome applications to control them or providing cool data insights into tech.
+        </h3>
+        <br />
+        <br />
+        <Links />
+      </div>
+      </MediaQuery>
+
+      <MediaQuery minDeviceWidth={1224}>
+      <div style={{textAlign: "center", marginTop: "10%", marginLeft: "10%", marginRight: "10%"}}>
+      <Typist cursor={{show: false}}>
+        <h1>Hello there!</h1>
+        </Typist>
+        <br/>
+        <br/>
+        <h3 style={{}}>
+          I'm Eli Verbrugge <img src={Me} alt="Avatar" class="avatar"/>, a Computer Science and Computer Engineering graduate interested in building cool tech whether it be
+          robots, awesome applications to control them or providing cool data insights into tech.
+        </h3>
+        <br />
+        <br />
+        <Links />
+      </div>
+      </MediaQuery>
   </div>
 );
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { Card, Jumbotron, Container } from "react-bootstrap";
-import Garmin from "../images/garmin.jpg"
+import { Card } from "react-bootstrap";
 import Rover from "../images/rover_swoosh.ico"
 import Laptop from "../images/rover_laptop.jpg"
 import AR from "../images/ar_tag.png"
@@ -15,14 +14,17 @@ const Projects = () => (
                 <Card.Title>Mars Rover Design Team <img src={Rover} alt="Avatar" class="avatar" /></Card.Title>
                 <Card.Text style={{textAlign:'left'}}>
                 <br/>
-                    Development lead for the Rover autonomy software stack designed for a teleoperated mars rover powered by a asynchronous state machine written in Python. Also software architect for one year, introduced robust TCP-backed error reporting to our network stack and architected our new brushless motor control framework for our robotic arm.  <br/>
-                    <br/>• Leading computer vision development using image classifiers as well as obstacle avoidance with depth map segmentation and clustering.
+                    Filled in multiple roles for my university's Mars Rover Design Team over the course of a couple years. 
+                    I was a software architect for one year, when I introduced robust TCP-backed error reporting to our network stack and architected our new brushless motor control framework for our robotic arm. 
+                    My last year I was the development lead for the Rover autonomy stack which was powered by a asynchronous state machine written in Python. <br/>
+                    <br/>• Led computer vision development using image classifiers as well as obstacle avoidance with depth map segmentation and clustering.
                     <br/>• Developed new team simulation and unit testing tools with continuous integration, and robust logging systems to track regressions.
+                    <br/>• I rewrote the C++ embedded software stack for our robotic arm which involved involved developing our motion planning software and studying our inverse kinematics models.
                     <br/>• Worked on improving software quality through code standards and reviews. 
                 </Card.Text>
                 </Card.Body>
-                <Card.Img variant="bottom" src={Laptop} style={{  paddingLeft: 50, paddingRight: 50}}/>
-                <Card.Img variant="bottom" src={AR} style={{  paddingLeft: 50, paddingRight: 50}}/>
+                <Card.Img variant="bottom" src={Laptop} style={{paddingLeft: "2vh", paddingRight: "2vh"}}/>
+                <Card.Img variant="bottom" src={AR} style={{paddingLeft: "2vh", paddingRight: "2vh"}}/>
                 <Card.Footer>
                     <small className="text-muted">August 2017 - May 2021</small>
                 </Card.Footer>
